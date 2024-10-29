9bef3a9ff210ad3898da15ec9e4ee42649cfe954
Abhishek Mazumder <ABHISHEKMAZUMDER72@gmail.com>
2024-10-29 23:11:18 +0530

    finally

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDi-bbg3KEwHOH8qOdHJRLtFcc8eVv-bMk",
-    authDomain: "bvec-code-club-dev.firebaseapp.com",
-    projectId: "bvec-code-club-dev",
-    storageBucket: "bvec-code-club-dev.appspot.com",
-    messagingSenderId: "763790961846",
-    appId: "1:763790961846:web:a4359b676e600b1f69ca17",
-    measurementId: "G-ML722EKFPP"
+const firebaseConfig = { // COPY FIREBASE CONFIG, AT FIREBASE CONFIG DON't ALLOW WRITE ONLY ALLOW READ
+                         // CONFIG WILL BE OPENLY VISIBLE BUT AS LONG AS WRITE IS NOT ALLOWED EVERYTHING SHOULD BE FINE(famous last words)
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
